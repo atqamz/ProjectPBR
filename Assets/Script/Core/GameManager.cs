4b67dc0d45726aedc3bc46d68456ae19c4756926
Atqa Munzir <atqamz@gmail.com>
2025-04-21 16:05:46 +0700

    feat: minimal order mechanism

---
+++ Assets/Script/Core/GameManager.cs
@@ -0,0 +1,7 @@
+namespace Script.Core
+{
+    public class GameManager
+    {
+        
+    }
+}
---
+++ Assets/Script/Pesan/AcceptOrderUI.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Script.Pesan
+{
+    public class AcceptOrderUI : MonoBehaviour
+    {
+        [SerializeField] Button button;
+        
+        
+    }
+}
